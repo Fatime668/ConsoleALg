004acf48abacde968ba00eae5df182427aa047ad
Fatime668 <tu7dkjdue@code.edu.az>
2022-03-03 22:04:52 +0400

    Console

---
+++ Algorithm/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter the number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number%2==1)
+            {
+                Console.WriteLine("It is a odd number");
+            }
+             else
+            {
+                Console.WriteLine("It is a even number");
+            }
+        }
+    }
+}
---
+++ Algorithm2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Algorithm2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter the first number");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please, enter the second number");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            if (number1 > number2)
+            {
+                Console.WriteLine(number1 +"is"+ " larger than " + number2);
+            }else if (number2 > number1)
+            {
+                Console.WriteLine(number2 + " is" + " larger than " + number1);
+            }
+            else
+            {
+                Console.WriteLine("The numbers are equal");
+            }
+        }
+    }
+}
